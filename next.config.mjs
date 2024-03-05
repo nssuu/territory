0c50feb2ea9c87178ff087f289f6b602d6ea01e5
nssuu <sukeremoglu@gmail.com>
2024-03-05 05:48:41 +0300

    Add recording, spinner

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: process.env.NODE_ENV === "development",
+};
 
 export default nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,17 @@
-import CardContent from "../components/CardContent/CardContent";
+"use client";
+
+import axios from "axios";
+import useSWR from "swr";
+import CardContent from "@/components/CardContent/CardContent";
 import CardData from "@/components/CardContent/interface/CardData";
 import converters from "@/components/utils/converter";
-import axios from "axios";
 
-async function fetchGaleryData(): Promise<CardData[]> {
+const BASE_API_URL = "http://localhost:4000";
+const HOUSES_API_URL = `${BASE_API_URL}/houses`;
+
+async function fetchGaleryData(url: string): Promise<CardData[]> {
   try {
-    const response = await axios.get<Record<string, unknown>[]>(
-      "http://localhost:4000/houses",
-    );
+    const response = await axios.get<Record<string, unknown>[]>(url);
     if (response.status === 200) {
       return converters.convertJsonToCardData(response.data);
     }
@@ -17,14 +21,24 @@ async function fetchGaleryData(): Promise<CardData[]> {
   return [];
 }
 
-export default async function Page() {
-  const galeryData = await fetchGaleryData();
+export default function Page() {
+  const {
+    data: galeryData,
+    error,
+    isLoading,
+  } = useSWR(HOUSES_API_URL, fetchGaleryData);
 
   return (
     <div className="grid place-items-center h-screen p-4">
-      {galeryData.map((cardData) => {
-        return <CardContent key={cardData.id} data={cardData} />;
-      })}
+      {isLoading ? (
+        <div>Loading...</div>
+      ) : error ? (
+        <div>Error loading data</div>
+      ) : galeryData != null ? (
+        galeryData.map((cardData) => {
+          return <CardContent key={cardData.id} data={cardData} />;
+        })
+      ) : null}
     </div>
   );
 }
